227fc245173f2ab7ea0ef825360322f9e9e194fb
KonstantIMP <mihedovkos@gmail.com>
2021-11-09 22:34:28 +0300

    Add some math operands

--- src/org/condit/kimp/Matrix.java
+++ src/org/condit/kimp/Matrix.java
@@ -30,5 +30,43 @@ public class Matrix {
     public Matrix(Matrix c){
         this(c.p);
     }
+
+    public Matrix sum(Matrix m) throws Exception {
+        if ((p.length != m.p.length) || (p.length != 0 && p[0].length != m.p[0].length)) throw new Exception("Incorrect matrix size");
+        int [][] res = new int[p.length][p[0].length];
+
+        for (int i = 0; i < p.length; i++) {
+            for (int j = 0; j < p[i].length; j++) {
+                res[i][j] = p[i][j] + m.p[i][j];
+            }
+        }
+
+        return new Matrix(res);
+    }
+
+    public Matrix sub(Matrix m) throws Exception {
+        if ((p.length != m.p.length) || (p.length != 0 && p[0].length != m.p[0].length)) throw new Exception("Incorrect matrix size");
+        int [][] res = new int[p.length][p[0].length];
+
+        for (int i = 0; i < p.length; i++) {
+            for (int j = 0; j < p[i].length; j++) {
+                res[i][j] = p[i][j] - m.p[i][j];
+            }
+        }
+
+        return new Matrix(res);
+    }
+
+    public Matrix mul(int m){
+        int [][] res = new int[p.length][p.length != 0 ? p[0].length : 0];
+
+        for (int i = 0; i < p.length; i++) {
+            for (int j = 0; j < p[i].length; j++) {
+                res[i][j] = p[i][j] * m;
+            }
+        }
+
+        return new Matrix(res);
+    }
     
 }

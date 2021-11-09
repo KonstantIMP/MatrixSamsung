7c0db01f63c5ab51c60f22d5a0906f9c0f6ca6c0
KonstantIMP <mihedovkos@gmail.com>
2021-11-09 22:41:33 +0300

    Add dimension field

--- src/org/condit/kimp/Matrix.java
+++ src/org/condit/kimp/Matrix.java
@@ -2,13 +2,17 @@ package org.condit.kimp;
 
 public class Matrix {
     private int [][] p;
+    private Dimension dimension;
 
     public Matrix() {
         p = new int[0][0];
+        dimension = new Dimension(0, 0);
     }
 
     public Matrix(int [] m, int n) {
         p = new int[m.length / n][n];
+        dimension = new Dimension(m.length / n, n);
+
         for(int i = 0; i < m.length; i++){
             p[i / n][i % n] = m[i];
         }
@@ -17,9 +21,13 @@ public class Matrix {
     public Matrix(int [][] m){
         if(m.length == 0) {
             p = new int[0][0];
+            dimension = new Dimension(0, 0);
             return;
         }
-        p = new int[m.length][m[0].length];
+
+        p = new int[m.length][m.length > 0 ? m[0].length : 0];
+        dimension = new Dimension(m.length, m.length > 0 ? m[0].length : 0);
+
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++){
                 p[i][j] = m[i][j];
@@ -68,5 +76,22 @@ public class Matrix {
 
         return new Matrix(res);
     }
-    
+
+
+    private static class Dimension {
+        private int rows;
+        private int columns;
+
+        public Dimension(int r, int c){
+            rows = r; columns = c;
+        }
+
+        public int getRows() {
+            return rows;
+        }
+
+        public int getColumns() {
+            return columns;
+        }
+    }
 }

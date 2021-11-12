fe479c9efea59741fa09455734d87dfc924e887e
KonstantIMP <mihedovkos@gmail.com>
2021-11-12 23:02:37 +0300

    Comments

--- src/org/condit/kimp/Matrix.java
+++ src/org/condit/kimp/Matrix.java
@@ -6,6 +6,9 @@ public class Matrix {
     private int [][] p;
     private Dimension dimension;
 
+    /**
+     * Creates a n*m matrix (all elements are 0)
+     */
     public static Matrix createNull(int n, int m) {
         int [][] res = new int[n][m];
 
@@ -18,6 +21,9 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Creates a n*n matrix (all elements are 0 except elements on the main diagonal)
+     */
     public static Matrix createUnit(int n) {
         int [][] res = new int[n][n];
 
@@ -31,6 +37,9 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Creates a n*m matrix (all elements are 0 or 1 - random)
+     */
     public static Matrix genRandomLogical(int n, int m) {
         int [][] res = new int[n][m];
 
@@ -45,6 +54,11 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Creates a matrix by a Range
+     * Contain all elements in [first; last]
+     * Has n elements per row
+     */
     public static Matrix genRange(int first, int last, int n) throws Exception {
         if ((last - first + 1) % n != 0) throw new Exception("Impossible num of elements per line");
 
@@ -54,6 +68,11 @@ public class Matrix {
         return new Matrix(tmp, n);
     }
 
+    /**
+     * Creates a matrix by a Range (but fills it by a snake)
+     * Contain all elements in [first; last]
+     * Has n elements per row
+     */
     public static Matrix genSnake(int first, int last, int n) throws Exception {
         if ((last - first + 1) % n != 0) throw new Exception("Impossible num of elements per line");
 
@@ -71,6 +90,10 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Swap the Matrix axes
+     * @return Matrix with the swapped axes
+     */
     public Matrix transpose() {
         int [][] res = new int[dimension.getColumns()][dimension.getRows()];
 
@@ -83,6 +106,9 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Check if the matrix is diagonal
+     */
     public boolean isDiagonal() {
         if (dimension.getColumns() != dimension.getRows()) return false;
 
@@ -99,17 +125,27 @@ public class Matrix {
         return true;
     }
 
+    /**
+     * Calculates arithmetic mean of the elements
+     */
     public double mean() throws Exception {
         if (dimension.getColumns() * dimension.getColumns() == 0)
             throw new Exception("Empty matrix");
         return (double)this.sum() / (double)(dimension.getColumns() * dimension.getColumns());
     }
 
+    /**
+     * Creates a new 0*0 Matrix
+     */
     public Matrix() {
         p = new int[0][0];
         dimension = new Dimension(0, 0);
     }
 
+    /**
+     * Creates a new matrix with all elements from m array
+     * Has n elements per row
+     */
     public Matrix(int [] m, int n) {
         p = new int[m.length / n][n];
         dimension = new Dimension(m.length / n, n);
@@ -119,6 +155,9 @@ public class Matrix {
         }
     }
 
+    /**
+     * Creates a new Matrix from two-dimensional array
+     */
     public Matrix(int [][] m){
         if(m.length == 0) {
             p = new int[0][0];
@@ -136,14 +175,23 @@ public class Matrix {
         }
     }
 
+    /**
+     * Copy ctor
+     */
     public Matrix(Matrix c){
         this(c.p);
     }
 
+    /**
+     * Returns the size of the Matrix
+     */
     public Dimension getDimension() {
         return dimension;
     }
 
+    /**
+     * Calculates a sum of 2 matrices
+     */
     public Matrix sum(Matrix m) throws Exception {
         if (dimension.getColumns() != m.dimension.getColumns() || dimension.getRows() != m.dimension.getRows()) throw new Exception("Incorrect matrix size");
         int [][] res = new int[dimension.getRows()][dimension.getColumns()];
@@ -157,6 +205,9 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Calculates a sub of 2 matrices
+     */
     public Matrix sub(Matrix m) throws Exception {
         if (dimension.getColumns() != m.dimension.getColumns() || dimension.getRows() != m.dimension.getRows()) throw new Exception("Incorrect matrix size");
         int [][] res = new int[dimension.getRows()][dimension.getColumns()];
@@ -170,6 +221,9 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Multiplies the matrix by m
+     */
     public Matrix mul(int m){
         int [][] res = new int[dimension.getRows()][dimension.getColumns()];
 
@@ -182,6 +236,9 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Divides the matrix by m(by elements)
+     */
     public Matrix div(int n) {
         int [][] res = new int[dimension.getRows()][dimension.getColumns()];
 
@@ -194,6 +251,9 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Search n in the matrix and return it`s index
+     */
     public int search(int n){
         int index = -1;
 
@@ -208,14 +268,23 @@ public class Matrix {
         return index;
     }
 
+    /**
+     * Returns index of the max element of the Matrix
+     */
     public int argmax() {
         return search(max());
     }
 
+    /**
+     * Returns index of the min element of the Matrix
+     */
     public int argmin() {
         return search(min());
     }
 
+    /**
+     * Multiply 2 matrices
+     */
     public Matrix mul(Matrix m) throws Exception {
         if (dimension.getColumns() != m.dimension.getRows())
             throw new Exception("Cannot mul matrix with this size");
@@ -235,6 +304,9 @@ public class Matrix {
         return new Matrix(res);
     }
 
+    /**
+     * Return max element of the Matrix
+     */
     public int max() {
         if (dimension.getColumns() == 0 || dimension.getRows() == 0) return 0;
         int m = p[0][0];
@@ -246,6 +318,9 @@ public class Matrix {
         return m;
     }
 
+    /**
+     * Return min element of the Matrix
+     */
     public int min() {
         if (dimension.getColumns() == 0 || dimension.getRows() == 0) return 0;
         int m = p[0][0];
@@ -257,6 +332,9 @@ public class Matrix {
         return m;
     }
 
+    /**
+     * Calculates sum of the elements
+     */
     public int sum() {
         int s = 0;
 
@@ -269,6 +347,9 @@ public class Matrix {
         return s;
     }
 
+    /**
+     * Calculates sum of the main diagonal elements
+     */
     public int trace() throws Exception {
         if (dimension.getColumns() != dimension.getRows())
             throw new Exception("Cannot trace non square matrix");
@@ -282,6 +363,9 @@ public class Matrix {
         return t;
     }
 
+    /**
+     * Returns element by index
+     */
     public int getElement(int index) throws Exception {
         if (index >= dimension.getRows() + dimension.getColumns())
             throw new Exception("Incorrect element index");
@@ -289,12 +373,15 @@ public class Matrix {
         return p[index / dimension.getColumns()][index % dimension.getColumns()];
     }
 
+    /**
+     * Returns element by it`s position
+     */
     public int getElement(int x, int y) throws Exception {
         if (x >= dimension.getColumns() || y >= dimension.getRows())
             throw new Exception("Incorrect element index");
         return this.getElement(x + y * dimension.getColumns());
     }
-
+    
     @Override
     public String toString(){
         StringBuilder builder = new StringBuilder();
